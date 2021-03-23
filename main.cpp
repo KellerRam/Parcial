429d89aefb71fe8251dd4beb0cbbcaf36c18edb7
Subzero <72477458+KellerRam@users.noreply.github.com>
2021-03-23 17:10:33 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+//Keller David Ram[irez Gramajo
 int main()
 {
+  int civil, industrial, sistemas;
   std::cout << "Propuesta de programa para el simposio de Ingenieria 2021\n";
+  std::cout << "Ingrese la cantidad de estudiantes de ingernier[ia civil";
+  std::cin >> civil;
+  std::cout << "Ingrese la cantidad de estudiantes de ingernier[ia civil";
+  std::cin >> industrial;
+  std::cout << "Ingrese la cantidad de estudiantes de ingernier[ia civil";
+  std::cin >> sistemas;
+  std::if(civil > industrial && civil > sistemas){
+    cout << "Ingenier[ia civil tendr[a el sal[on m[as grande"
+  }
+  std::else if(indutrial > civil &&  industrial > sistemas){
+    cout << "Industrial sal[on m[as grande"
+  }
   std::cin.ignore();
   return 0;
 }
